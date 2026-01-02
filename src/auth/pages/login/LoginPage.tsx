d3f1630b9afd0ee049f927d3b09572b0383ea91c
David <dmpuerto23@gmail.com>
2026-01-02 11:59:08 -0300

    commit inicial de rama

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -1,3 +1,4 @@
+import type { FormEvent } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
@@ -6,11 +7,23 @@ import { CustomLogo } from "@/components/custom/CustomLogo"
 import { Link } from "react-router"
 
 export const LoginPage = () => {
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // busca todos los elementos que tengan atributo name
+    const formData = new FormData(event.target as HTMLFormElement);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    
+    
+  }
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleLogin}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <CustomLogo />
@@ -18,7 +31,7 @@ export const LoginPage = () => {
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Correo</Label>
-                <Input id="email" type="email" placeholder="mail@example.com" required />
+                <Input id="email" type="email" name="email" placeholder="mail@example.com" required />
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
@@ -27,7 +40,7 @@ export const LoginPage = () => {
                     ¿Olvidaste tu contraseña?
                   </a>
                 </div>
-                <Input id="password" type="password" required placeholder="Contraseña" />
+                <Input id="password" type="password" name="password" required placeholder="Contraseña" />
               </div>
               <Button type="submit" className="w-full">
                 Login
